4aa3ad0201aa91e9a4aa8aab7b9b919f03c7b189
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-08-14 10:48:46 -0500

    spring boot maven base

---
+++ src/test/java/com/clarocol/mongoLoadTest/MongoLoadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarocol.mongoLoadTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoLoadTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
